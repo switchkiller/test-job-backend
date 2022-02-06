c536667eca621f922acdd0c05747f21adeca23f4
Barun Singhania <emailbarung@gmail.com>
2022-02-07 01:22:32 +0530

    temporarly removing pipeline integration

--- packages/infrastructure/lib/infrastructure-stack.ts
+++ packages/infrastructure/lib/infrastructure-stack.ts
@@ -1,21 +1,26 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
+import { ApolloBasedService } from './apollo-based-graphql-service';
 import { PipelineGraphQlApiStage } from './pipeline-stage';
 
 export class InfrastructureStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
     
-    const pipeline = new CodePipeline(this, 'Pipeline', {
-      pipelineName: "IdeathonPipeline",
-      synth: new ShellStep("Synth", {
-        input: CodePipelineSource.gitHub('switchkiller/test-job-backend', 'master'),
-        commands: ['npm ci', 'npm run build', 'npx cdk synth']
-      })
-    })
+    // const pipeline = new CodePipeline(this, 'Pipeline', {
+    //   pipelineName: "IdeathonPipeline",
+    //   synth: new ShellStep("Synth", {
+    //     input: CodePipelineSource.gitHub('switchkiller/test-job-backend', 'master'),
+    //     commands: ['npm ci', 'npm run build', 'npx cdk synth']
+    //   })
+    // })
 
-    const UAT = pipeline.addStage(new PipelineGraphQlApiStage(this, "Test"));
+    // const UAT = pipeline.addStage(new PipelineGraphQlApiStage(this, "Test"));
+
+    const jobService = new ApolloBasedService(this, "JobService", {
+      serviceName: "jobs-service"
+    });
 
   }
 }
